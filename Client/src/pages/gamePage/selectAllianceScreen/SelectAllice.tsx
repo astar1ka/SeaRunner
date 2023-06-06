d2128e01f75ec454cee86621012c2533ee5b6372
astar1ka <astar1ka@mail.ru>
2023-06-07 02:42:51 +0400

    select alliance image

--- Client/src/pages/gamePage/selectAllianceScreen/SelectAllice.tsx
+++ Client/src/pages/gamePage/selectAllianceScreen/SelectAllice.tsx
@@ -1,16 +1,24 @@
-export default function SelectAllice(props: any){
-    const allianceid = props.id;
-    const allianceName = (allianceid === 1) ? 'Компания':
-    (allianceid === 2) ? 'Армада':
-    (allianceid === 3) ? 'Альянс': ''
+import flag1 from '../../../source/1.jpg';
+import flag2 from '../../../source/2.jpg';
+import flag3 from '../../../source/3.jpg';
+
+
+export default function SelectAllice(props: any) {
+    const allianceid: number = props.id;
+    const allianceName = (allianceid === 1) ? 'Компания' :
+        (allianceid === 2) ? 'Армада' :
+            (allianceid === 3) ? 'Альянс' : ''
+    const img = (allianceid === 1) ? flag1 :
+        (allianceid === 2) ? flag2 :
+            (allianceid === 3) ? flag3 : ''
     const callback = props.callback;
+
 
     const onClickHandler = () => {
         callback(props.id);
     }
 
-    return (<div className='selectAllice' onClick={onClickHandler}>
-        <img className={'image' + allianceid}/>
-        <a>{allianceName}</a>
+    return (<div className={'selectAllice '+`alliance${allianceid}`} onClick={onClickHandler}>
+        <div className='nameAlliance'>{allianceName}</div>
     </div>)
 }
